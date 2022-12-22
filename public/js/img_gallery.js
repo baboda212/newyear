250ac784c99514dc04f42226ace56796d5e2e640
baboda212 <baboda212@naver.com>
2022-12-22 22:41:57 +0900

    변경내용

--- public/js/img_gallery.js
+++ public/js/img_gallery.js
@@ -1,13 +1,13 @@
-$(function(){
-    setInterval(function(){
-        $('.slide div:first-child')
-        .fadeIn(1000)
-        .next('div')
-        .fadeOut(1000)
-        .end()
-        .appendTo('.slide')
-    },5000);
-});
+let num = 0;
+setInterval (function(){
+    num++;
+    if(num >= 3) {
+        num = 0;
+    };
+    let imgTag = document.getElementById('photo');
+    imgTag.setAttribute('src', `images/img${num}.jpg`);
+},5000);
+
 let yearEl = document.getElementById('year');//년
 let monthEl = document.getElementById('month');//월
 let dateEl = document.getElementById('date');//일
@@ -16,10 +16,21 @@ let d = new Date();
 //년, 월, 일
 let year = d.getFullYear();
 let month = d.getMonth() + 1; //보정값 +1
-console.log(month);
+//console.log(month);
 let date = d.getDate();
-console.log(month,date);
+console.log(year,month,date);
 //년, 월,일 출력
-yearEl.innerHTML= year;
-monthEl.innerHTML= month;
-dateEl.innerHTML= date;
+yearEl.textContent= year;
+monthEl.textContent= month;
+dateEl.textContent= date;
+
+function len_chk(){  
+    let frm = document.review_box.desc; 
+      
+    if(frm.value.length > 510){  
+         alert("글자수는 한글255자로 제한됩니다.!");  
+         frm.value = frm.value.substring(0,510);  
+         frm.focus();  
+    } 
+  
+  }; 
